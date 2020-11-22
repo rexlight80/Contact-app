a7c6c7b1ebcbc26034af3907e825d3595f4be45f
Your Name <you@example.com>
2020-11-22 20:42:03 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+export const imageConfig = {
+    quality: 0.2,
+    maxWidth: 800,
+    maxHeight: 600,
+    autoRotate: true
+  };
+
+  export const firebaseConfig={
+    apiKey: "AIzaSyAoZ1OWWRKku9cCzKZv1ebonGl3K-QUnnI",
+    authDomain: "mygitapp-d8bb0.firebaseapp.com",
+    databaseURL: "https://mygitapp-d8bb0.firebaseio.com",
+    projectId: "mygitapp-d8bb0",
+    storageBucket: "mygitapp-d8bb0.appspot.com",
+    messagingSenderId: "298334706628",
+    appId: "1:298334706628:web:53f8527685d041218fe291",
+    measurementId: "G-GX32VL9CC1"
+  };
